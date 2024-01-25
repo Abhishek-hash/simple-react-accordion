2e90025784cdf7154aa6c795e56c408ef7d5198b
Abhishek Maurya <abhi790maurya@gmail.com>
2024-01-26 03:06:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [selected, setSelected] = useState(null)
+  console.log("selected:",selected)
+  const toggle = (i) => {
+    console.log("value of i in toggle:", i)
+    if(selected == i) return setSelected(null)
+    setSelected(i)
+  } 
+
+  return (
+    <div className="App">
+      <div >
+        <h1 style={{ color: 'green' }}>Simple Accordion</h1>
+      </div>
+
+      <div className='accordian'>
+
+        {
+          data.map((item, i) => {
+            return <div key={i} className='item'>
+              <div className='title'  onClick={() => toggle(i)}>
+                <h3> {item['question']}</h3>
+                <span style={{ fontSize: '30px' }}>
+                  {
+                    selected === i ? '-' : '+'
+                  }
+                </span>
+              </div>
+              <div className='content'>
+                
+                {
+                    selected === i ? item['answer'] : null
+                }
+              </div>
+            </div>;
+          })
+        }
+
+      </div>
+    </div>
+  );
+}
+
+const data = [
+  {
+    question: 'Question 1',
+    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    question: 'Question 2',
+    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    question: 'Question 3',
+    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    question: 'Question 4',
+    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    question: 'Question 5',
+    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    question: 'Question 6',
+    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+]
+
+export default App;
